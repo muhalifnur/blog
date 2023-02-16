47bbdb099e74cf963b2f18141cd194b9ba8f023b
muhalifnur <muhalifnur2002@gmail.com>
2023-02-16 14:16:24 +0800

    dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,9 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector(".to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 
@@ -49,4 +56,24 @@ form.addEventListener("submit", (e) => {
       console.log("Success!", response);
     })
     .catch((error) => console.error("Error!", error.message));
+});
+
+//klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+//Darkmode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+  } else {
+    html.classList.remove("dark");
+  }
 });
